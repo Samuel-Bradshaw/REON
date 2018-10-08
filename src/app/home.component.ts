d0c8044e5f260833aba4d15830a9dfc2c1ec9423
Samuel Bradshaw <zcapscb@ucl.ac.uk>
2018-10-08 15:58:50 +0100

    home page image aspect

--- src/app/home.component.ts
+++ src/app/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit,  ElementRef, ViewChild } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { DomSanitizer } from '@angular/platform-browser';
 
@@ -7,14 +7,23 @@ import { DomSanitizer } from '@angular/platform-browser';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
 
   constructor(private http: HttpClient, public sanitizer: DomSanitizer) { }
 
   page_components: any;
+  @ViewChild('homeimage') homeimage: ElementRef;
 
   ngOnInit() {
   	this.getPageInfo();
+  }
+
+  ngAfterViewInit(){
+
+
+
+
+
   }
 
 
@@ -34,6 +43,27 @@ export class HomeComponent implements OnInit {
           if(this.page_components.video_url !== null){
           		this.page_components.video_url =  "https://www.youtube.com/embed/"+this.page_components.video_url; 
       	  }
+          //add image to page
+            if(this.page_components.image_filepath !== null){
+
+                let img = document.createElement("img");
+                img.src = "http://localhost/REON/"+this.page_components.image_filepath;
+
+                let container = this.homeimage.nativeElement;
+                container.appendChild(img);
+                img.style.maxWidth = "100%";
+
+                img.addEventListener('load', function(){
+                let aspectRatio = img.clientWidth / img.clientHeight;
+
+                 if(aspectRatio > 1.1){
+                    container.classList.add("wideimg");
+                 }else{
+                    container.classList.add("tallimg");
+                   }
+                });
+
+               
    
        
         },(error: any) => { console.log(error);}
