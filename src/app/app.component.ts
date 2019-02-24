020a77c31abcdfdbec8aa5c3100f6be288eb1e11
Samuel Bradshaw <zcapscb@ucl.ac.uk>
2019-02-24 17:05:40 +0000

    css translate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,17 +24,14 @@ ngAfterViewInit(){
 
                         
   setTimeout(function(){
-    $("#google_translate_element").css("display", "inline-block");
 
+    $("#google_translate_element").css("display", "inline-block");
 
     $("#google_translate_element img").eq(0).css("background-image", "");
     $("#google_translate_element img").eq(0).attr("src", "./assets/images/google.jpg");
     $("#urlLeftIcon").val('');
 
     $("#google_translate_element img").eq(0).css("mix-blend-mode", "multiply");
-    $("#google_translate_element img").eq(0).css("height", "15px");
-    $("#google_translate_element img").eq(0).css("width", "15px");
-
 
     $("#google_translate_element span").css("border", "none");
     $("#google_translate_element div").eq(1).css("border", "none");
@@ -46,8 +43,10 @@ ngAfterViewInit(){
     $("#borderColor").val('');
 */
   var borderRadius = "3px";
-    $("#google_translate_element span").css("border-radius", borderRadius);
+   // $("#google_translate_element span").css("border-radius", borderRadius);
     $("#google_translate_element div").eq(1).css("border-radius", borderRadius);
+
+
 
   var backgroundColor = "rgba(200,200,200,0.9)";
  
@@ -63,12 +62,44 @@ ngAfterViewInit(){
     $("#google_translate_element a").css("text-shadow", "1px 1px 1px #000000");
 
     $("#google_translate_element a").css("font-family", "Questrial");
+
+    var width = $(window).width();
+    console.log(width);
+
+    var iconSize;
+    var textSize;
+
+    if(width > 740){
+      iconSize = "15px";
+      textSize = "12px";
   
-  var textSize = "12px";
-  
+     }  else if(width > 550){
+      iconSize = "12px";
+      textSize = "10px";
+      } else if (width > 350){
+      iconSize = "10px";
+      textSize = "8px";
+      } else{
+      iconSize = "9px";
+      textSize = "7px";
+      $("#google_translate_element a").css("margin", " 0 2px 0 0");
+
+      }
+
+  if (width < 320){
+    $("#google_translate_element").css("display", "none");
+  } else if(width < 480){
+    $("#google_translate_element span").eq(1).text("Language");
+    $("#centerText").val('');
+  } 
+
+    $("#google_translate_element img").eq(0).css("height", iconSize);
+    $("#google_translate_element img").eq(0).css("width", iconSize);
     $("#google_translate_element a").css("font-size", textSize);
+
+     $("#google_translate_element a").css("vertical-align", "middle");
+      $("#google_translate_element a").css("line-height", "normal");
     $("#textSize").val('');
-  
 } , 1000) ;          
 
 }
